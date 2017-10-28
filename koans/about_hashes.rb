e08ded9f839282b8cc7e67d53f8fc36cb096b3e5
gavvel <fernando_gabriel@hotmail.de>
2017-10-28 18:12:47 +0200

    meditated on 39

--- koans/about_hashes.rb
+++ koans/about_hashes.rb
@@ -55,15 +55,15 @@ class AboutHashes < Neo::Koan
     assert_equal 2, hash.keys.size
     assert_equal true, hash.keys.include?(:one)
     assert_equal true, hash.keys.include?(:two)
-    assert_equal Hash, hash.keys.class
+    assert_equal Array, hash.keys.class
   end
 
   def test_hash_values
     hash = { :one => "uno", :two => "dos" }
-    assert_equal __, hash.values.size
-    assert_equal __, hash.values.include?("uno")
-    assert_equal __, hash.values.include?("dos")
-    assert_equal __, hash.values.class
+    assert_equal 2, hash.values.size
+    assert_equal true, hash.values.include?("uno")
+    assert_equal true, hash.values.include?("dos")
+    assert_equal Array, hash.values.class
   end
 
   def test_combining_hashes
