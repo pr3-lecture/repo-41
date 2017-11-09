09679fbd7ec702ce7dea0909f004e165cb7d59b4
gavvel <fernando_gabriel@hotmail.de>
2017-11-09 13:00:43 +0100

    meditated over 155

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -15,6 +15,13 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if(a == b && b == c && a == c)
+    :equilateral
+  elsif (a != b && b != c && a != c)
+    :scalene
+  else
+    :isosceles
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
