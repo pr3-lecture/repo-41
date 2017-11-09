1bdf63573ca96054b38b4b58eed237b4195f271a
gavvel <fernando_gabriel@hotmail.de>
2017-11-09 10:14:11 +0100

    meditated over 124

--- koans/about_methods.rb
+++ koans/about_methods.rb
@@ -116,12 +116,12 @@ class AboutMethods < Neo::Koan
     assert_equal "a secret", my_private_method
   end
 
-#  def test_calling_private_methods_with_an_explicit_receiver
-#    exception = assert_raise("NoMethodError") do
-#      self.my_private_method
-#    end
-#    assert_match /./, exception.message
-#  end
+  def test_calling_private_methods_with_an_explicit_receiver
+    exception = assert_raise(NoMethodError) do
+      self.my_private_method
+    end
+    assert_match /./, exception.message
+  end
 
   # ------------------------------------------------------------------
 
@@ -144,7 +144,7 @@ class AboutMethods < Neo::Koan
 
   def test_calling_private_methods_in_other_objects
     rover = Dog.new
-    assert_raise("NoMethodError") do
+    assert_raise(NoMethodError) do
       rover.tail
     end
   end
